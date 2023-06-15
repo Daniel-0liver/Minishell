db1bf32f7abc77f310c9c269e6d23f85156af8bf
pro-gabrielteixeira <pro.gabrielteixeira@gmail.com>
2023-06-15 15:34:20 +0100

    Free for each malloc

--- src/cmd_handler/cmds.c
+++ src/cmd_handler/cmds.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cmds.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gateixei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 14:16:32 by gateixei          #+#    #+#             */
-/*   Updated: 2023/06/14 18:29:24 by dateixei         ###   ########.fr       */
+/*   Updated: 2023/06/15 15:04:02 by gateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,53 @@
 
 void    free_cmds(char ***cmds)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    i = 0;
-    while (cmds && cmds[i] != NULL)
-    {
-        j = 0;
-        while (cmds[i] && cmds[i][j] != NULL)
-        {
-            free(cmds[i][j]);
-            cmds[i][j] = NULL;
-            j++;
-        }
-        free(cmds[i]);
-        cmds[i] = NULL;
-        i++;
-    }
+	i = 0;
+	while (cmds && cmds[i] != NULL)
+	{
+		j = 0;
+		while (cmds[i] && cmds[i][j] != NULL)
+		{
+			free(cmds[i][j]);
+			cmds[i][j] = NULL;
+			j++;
+		}
+		free(cmds[i]);
+		cmds[i] = NULL;
+		i++;
+	}
 	free(cmds);
 	cmds = NULL;
 }
 
+void    free_test(void)
+{
+	int i;
+
+	i = 0;
+	while (data()->test && data()->test[i] != NULL)
+	{
+		free(data()->test[i]);
+		data()->test[i] = NULL;
+		i++;
+	}
+	free(data()->test);
+	data()->test = NULL;
+}
+
 void    free_fds(int **fds)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (fds && fds[i] != NULL)
-    {
-        free(fds[i]);
-        fds[i] = NULL;
-        i++;
-    }
+	i = 0;
+	while (fds && fds[i] != NULL)
+	{
+		free(fds[i]);
+		fds[i] = NULL;
+		i++;
+	}
 	free(fds);
 	fds = NULL;
 }
@@ -55,28 +70,30 @@ void    free_fds(int **fds)
 void	check_spc(void)
 {
 	generate_fds();
-	if (data()->test[data()->spc[data()->curr_spc]][0] == '|')
-	    ft_exec();
-    else if (data()->test[data()->spc[data()->curr_spc]][0] == '>')
-    {
-	    ft_red();
+	if (is_exec(data()->test[data()->spc[data()->curr_spc]]))
+		ft_exec();
+	else if (is_redirect(data()->test[data()->spc[data()->curr_spc]]))
+	{
+		ft_red();
 	}
 	while (data()->spc && data()->spc[data()->curr_spc] != '\0' && data()->spc[data()->curr_spc + 1] != '\0')
 	{
-		if (data()->test[data()->spc[data()->curr_spc]][0] == '|')
+		if (is_exec(data()->test[data()->spc[data()->curr_spc]]))
 			ft_exec_pipe_md();
-		else if (data()->test[data()->spc[data()->curr_spc]][0] == '>')
+		else if (is_redirect(data()->test[data()->spc[data()->curr_spc]]))
 			ft_red_end();
 		else
 			printf("Not pipe or >\n");
 	}
-	if (data()->spc[data()->curr_spc] == '\0')
-        return ;
-	if (data()->test[data()->spc[data()->curr_spc]][0] == '|')
+	if (is_exec(data()->test[data()->spc[data()->curr_spc - 1]]) && data()->spc[data()->curr_spc] == '\0')
 		ft_exec_pipe_end();
-	else if (data()->test[data()->spc[data()->curr_spc]][0] == '>')
+	else if (is_redirect(data()->test[data()->spc[data()->curr_spc]]))
 		ft_red_end();
-    else
+	else if (is_exec(data()->test[data()->spc[data()->curr_spc]]))
+		ft_exec_pipe_end();
+	else if (data()->spc[data()->curr_spc] == '\0')
+		return ;
+	else
 		printf("2Not pipe or >\n");
 }
 
@@ -96,9 +113,7 @@ void cmd_to_exec(void) // Main Fuction
 	if (data()->spc && data()->spc[data()->curr_spc] != '\0')
 	{
 		check_spc();
-		free_fds(data()->fd);
-		free(data()->spc);
-		data()->spc = NULL;
+		free_all();
 	}
 	else
 	{
@@ -112,8 +127,8 @@ void cmd_to_exec(void) // Main Fuction
 			else
 				waitpid(pid, NULL, 0);
 		}
+		free_exec();
 	}
-    free_cmds(data()->cmds);
 }
 
 // Check echo > vs echo ">"
--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gateixei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 20:52:54 by dateixei          #+#    #+#             */
-/*   Updated: 2023/06/14 17:02:12 by dateixei         ###   ########.fr       */
+/*   Updated: 2023/06/15 12:15:44 by gateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int	main(int argc, char *argv[], char **envp)
 		if (data()->str_cmd)
 			free(data()->str_cmd);
 		signal(SIGINT, inthandler);
-		data()->str_cmd = readline("minihell: ");
+		data()->str_cmd = readline("minishell: ");
 		add_history(data()->str_cmd);
         if (ft_strcpm(data()->str_cmd, "exit"))
             break;
